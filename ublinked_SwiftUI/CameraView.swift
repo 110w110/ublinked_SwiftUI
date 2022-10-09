d596976c60881f8dda95582159eb6f9bb331c9ea
110w110 <110w110.official@gmail.com>
2022-10-10 02:22:09 +0900

    아이콘 변경

---
+++ ublinked_SwiftUI/CameraView.swift
@@ -0,0 +1,144 @@
+//
+//  CameraView.swift
+//  ublinked_SwiftUI
+//
+//  Created by 한태희 on 2022/10/10.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct CameraView: View {
+    @ObservedObject var viewModel = CameraViewModel()
+
+        var body: some View {
+            ZStack {
+                viewModel.cameraPreview.ignoresSafeArea()
+                    .onAppear {
+                        viewModel.configure()
+                    }
+                
+                VStack(spacing: 0) {
+                    Spacer()
+                        .frame(height: 60)
+                    HStack(spacing: 0) {
+                        // 셔터사운드 온오프
+                        Button(action: {viewModel.switchFlash()}) {
+                            Image(systemName: viewModel.isFlashOn ?
+                                  "speaker.fill" : "speaker")
+                                .foregroundColor(viewModel.isFlashOn ? .yellow : .white)
+                                .frame(width: 15, height: 15)
+                        }
+                        .padding(.horizontal, 25)
+                        
+                        Spacer()
+                        
+                        // 플래시 온오프
+                        Button(action: {viewModel.switchSilent()}) {
+                            Image(systemName: viewModel.isSilentModeOn ?
+                                  "bolt.fill" : "bolt")
+                                .foregroundColor(viewModel.isSilentModeOn ? .yellow : .white)
+                                .frame(width: 15, height: 15)
+                        }
+                        .padding(.horizontal, 25)
+                        
+                        Spacer()
+                        
+                        Button(action: {viewModel.changeCamera()}) {
+                            Image(systemName: "arrow.triangle.2.circlepath.camera")
+                                .frame(width: 15, height: 15)
+                            
+                        }
+                        .padding(.horizontal, 25)
+                    }
+                    .font(.system(size:25))
+                    .padding()
+                    
+                    Spacer()
+                    
+                    ZStack(alignment: .bottom){
+                        Rectangle()
+                            .fill(Color.black)
+                            .opacity(0.5)
+                            .frame(height: 140)
+//
+                        HStack(alignment: .center){
+                            // 찍은 사진 미리보기, 일단 액션 X
+                            Button(action: {}) {
+                                Rectangle()
+                                    .stroke(lineWidth: 2)
+                                    .frame(width: 50, height: 50)
+                                    .padding(15)
+                            }
+                            
+                            Spacer()
+                            
+                            // 사진찍기 버튼
+                            ZStack{
+                                Circle()
+                                    .stroke(lineWidth: 5)
+                                    .frame(width: 65, height: 65)
+                                    .padding()
+                                
+                                Button(action: {viewModel.capturePhoto()}) {
+                                    Circle()
+                                        .fill(Color.white)
+                                        .frame(width: 55, height: 55)
+                                        .padding(15)
+                                }
+                            }
+                            
+                            Spacer()
+                            
+                            Button(action: {}) {
+                                Rectangle()
+                                    .stroke(lineWidth: 2)
+                                    .frame(width: 50, height: 50)
+                                    .padding(15)
+                                    .opacity(0)
+                            }
+                        }
+                        .padding(30)
+                    }
+                }
+                .foregroundColor(.white)
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
+}
+
+struct CameraPreviewView: UIViewRepresentable {
+    class VideoPreviewView: UIView {
+        override class var layerClass: AnyClass {
+             AVCaptureVideoPreviewLayer.self
+        }
+        
+        var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+            return layer as! AVCaptureVideoPreviewLayer
+        }
+    }
+    
+    let session: AVCaptureSession
+   
+    func makeUIView(context: Context) -> VideoPreviewView {
+        let view = VideoPreviewView()
+        
+        view.videoPreviewLayer.session = session
+        view.backgroundColor = .black
+        view.videoPreviewLayer.videoGravity = .resizeAspectFill
+        view.videoPreviewLayer.cornerRadius = 0
+        view.videoPreviewLayer.connection?.videoOrientation = .portrait
+
+        return view
+    }
+    
+    func updateUIView(_ uiView: VideoPreviewView, context: Context) {
+        
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
---
+++ ublinked_SwiftUI/CameraViewModel.swift
@@ -0,0 +1,95 @@
+//
+//  CameraViewModel.swift
+//  ublinked_SwiftUI
+//
+//  Created by 한태희 on 2022/10/10.
+//
+
+import SwiftUI
+import AVFoundation
+
+class CameraViewModel: ObservableObject {
+    private let model: Camera
+    private let session: AVCaptureSession
+    let cameraPreview: AnyView
+
+    @Published var isFlashOn = false
+    @Published var isSilentModeOn = false
+    
+    func configure() {
+        model.requestAndCheckPermissions()
+    }
+    
+    func switchFlash() {
+        isFlashOn.toggle()
+    }
+    
+    func switchSilent() {
+        isSilentModeOn.toggle()
+    }
+    
+    func capturePhoto() {
+        print("[CameraViewModel]: Photo captured!")
+    }
+    
+    func changeCamera() {
+        print("[CameraViewModel]: Camera changed!")
+    }
+    
+    init() {
+        model = Camera()
+        session = model.session
+        cameraPreview = AnyView(CameraPreviewView(session: session))
+    }
+}
+
+class Camera: ObservableObject {
+    var session = AVCaptureSession()
+    var videoDeviceInput: AVCaptureDeviceInput!
+    let output = AVCapturePhotoOutput()
+    
+    // 카메라 셋업 과정을 담당하는 함수, positio
+    func setUpCamera() {
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                for: .video, position: .back) {
+            do { // 카메라가 사용 가능하면 세션에 input과 output을 연결
+                videoDeviceInput = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(videoDeviceInput) {
+                    session.addInput(videoDeviceInput)
+                }
+                
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                    output.isHighResolutionCaptureEnabled = true
+                    output.maxPhotoQualityPrioritization = .quality
+                }
+                session.startRunning() // 세션 시작
+            } catch {
+                print(error) // 에러 프린트
+            }
+        }
+    }
+    
+    func requestAndCheckPermissions() {
+        // 카메라 권한 상태 확인
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            // 권한 요청
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] authStatus in
+                if authStatus {
+                    DispatchQueue.main.async {
+                        self?.setUpCamera()
+                    }
+                }
+            }
+        case .restricted:
+            break
+        case .authorized:
+            // 이미 권한 받은 경우 셋업
+            setUpCamera()
+        default:
+            // 거절했을 경우
+            print("Permession declined")
+        }
+    }
+}
