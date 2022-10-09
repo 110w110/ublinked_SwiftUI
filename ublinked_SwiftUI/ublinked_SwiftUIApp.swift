902b732ff4ad014452cb4ea635e3fc46a1e92edd
110w110 <110w110.official@gmail.com>
2022-10-09 20:32:09 +0900

    Initial Commit

---
+++ ublinked_SwiftUI/ublinked_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ublinked_SwiftUIApp.swift
+//  ublinked_SwiftUI
+//
+//  Created by 한태희 on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct ublinked_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
